30cfd4156327b102a5c7ff7d271e260accaea798
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-11-02 01:42:57 +0800

    Home Ribbon

--- AllAboutTeethDCMS/AttachedProperties/ButtonProperties.vb
+++ AllAboutTeethDCMS/AttachedProperties/ButtonProperties.vb
@@ -10,4 +10,15 @@
     Public Shared Function GetImage(ByVal button As DependencyObject) As ImageSource
         Return button.GetValue(ImageProperty)
     End Function
+
+    Public Shared ReadOnly SubImageProperty As DependencyProperty = DependencyProperty.RegisterAttached("SubImage", GetType(ImageSource), GetType(ButtonProperties), New PropertyMetadata(New ImageBrush().ImageSource))
+
+    Public Shared Sub SetSubImage(ByVal button As DependencyObject, ByVal value As ImageSource)
+        button.SetValue(SubImageProperty, value)
+    End Sub
+
+    Public Shared Function GetSubImage(ByVal button As DependencyObject) As ImageSource
+        Return button.GetValue(SubImageProperty)
+    End Function
+
 End Class
