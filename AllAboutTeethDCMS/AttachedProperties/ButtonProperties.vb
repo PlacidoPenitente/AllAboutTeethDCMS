68d5dfb967c011456c943eac152caa391102d22c
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-11-01 18:37:52 +0800

    Attached Property!

---
+++ AllAboutTeethDCMS/AttachedProperties/ButtonProperties.vb
@@ -0,0 +1,13 @@
+﻿Public Class ButtonProperties
+    Inherits DependencyObject
+
+    Public Shared ReadOnly ImageProperty As DependencyProperty = DependencyProperty.RegisterAttached("Image", GetType(ImageSource), GetType(ButtonProperties), New PropertyMetadata(New ImageBrush().ImageSource))
+
+    Public Shared Sub SetImage(ByVal button As DependencyObject, ByVal value As ImageSource)
+        button.SetValue(ImageProperty, value)
+    End Sub
+
+    Public Shared Function GetImage(ByVal button As DependencyObject) As ImageSource
+        Return button.GetValue(ImageProperty)
+    End Function
+End Class
