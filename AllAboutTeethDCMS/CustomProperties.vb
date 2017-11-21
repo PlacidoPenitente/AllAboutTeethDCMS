79227a6e5cee114a97127669ca99af5c5eb7455c
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-11-21 15:13:01 +0800

    Added AddAccount

--- AllAboutTeethDCMS/CustomProperties.vb
+++ AllAboutTeethDCMS/CustomProperties.vb
@@ -13,12 +13,22 @@
 
     Public Shared ReadOnly VisibilityProperty As DependencyProperty = DependencyProperty.RegisterAttached("Visibility", GetType(Visibility), GetType(CustomProperties), New PropertyMetadata(Visibility.Visible))
 
-    Public Shared Sub SetVisibility(ByVal ribbon As DependencyObject, ByVal value As Visibility)
-        ribbon.SetValue(VisibilityProperty, value)
+    Public Shared Sub SetVisibility(ByVal element As DependencyObject, ByVal value As Visibility)
+        element.SetValue(VisibilityProperty, value)
     End Sub
 
-    Public Shared Function GetVisibility(ByVal ribbon As DependencyObject) As Visibility
-        Return ribbon.GetValue(VisibilityProperty)
+    Public Shared Function GetVisibility(ByVal element As DependencyObject) As Visibility
+        Return element.GetValue(VisibilityProperty)
+    End Function
+
+    Public Shared ReadOnly LabelProperty As DependencyProperty = DependencyProperty.RegisterAttached("Label", GetType(String), GetType(CustomProperties), New PropertyMetadata(""))
+
+    Public Shared Sub SetLabel(ByVal element As DependencyObject, ByVal value As String)
+        element.SetValue(LabelProperty, value)
+    End Sub
+
+    Public Shared Function GetLabel(ByVal element As DependencyObject) As String
+        Return element.GetValue(LabelProperty)
     End Function
 
 End Class
---
+++ AllAboutTeethDCMS/Pages/AddAccount.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class AddAccount
+
+End Class
