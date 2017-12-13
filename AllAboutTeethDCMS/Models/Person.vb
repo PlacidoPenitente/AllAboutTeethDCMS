4cba03ab310e97f0d0c3abda2d936cc2dac4f699
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-13 10:56:16 +0800

    More Bindings.

--- AllAboutTeethDCMS/Models/Person.vb
+++ AllAboutTeethDCMS/Models/Person.vb
@@ -1,20 +1,59 @@
 ﻿Public Class Person
-    Private family_name As String = "Estrera"
+    Private m_familyname As String = "Estrera"
+    Private m_givenname As String = "Jay Mark"
+    Private m_middleinitial As String = "P"
+    Private m_gender As String = "Male"
+    Private m_birthdate As Date = Date.Now
+
 
     Public Property FamilyName As String
         Get
-            Return family_name
+            Return m_familyname
         End Get
         Set(value As String)
-            family_name = value
+            m_familyname = value
         End Set
     End Property
+
     Public Property GivenName As String
+        Get
+            Return m_givenname
+        End Get
+        Set(value As String)
+            m_givenname = value
+        End Set
+    End Property
+
     Public Property MiddleInitial As String
+        Get
+            Return m_middleinitial
+        End Get
+        Set(value As String)
+            m_middleinitial = value
+        End Set
+    End Property
+
     Public Property Birthdate As Date
+        Get
+            Return m_birthdate
+        End Get
+        Set(value As Date)
+            m_birthdate = value
+        End Set
+    End Property
+
     Public Property Gender As String
+        Get
+            Return m_gender
+        End Get
+        Set(value As String)
+            m_gender = value
+        End Set
+    End Property
+
     Public Property ContactNo As String
     Public Property Address As String
     Public Property EmailAddress As String
     Public Property Image As String
+
 End Class
--- AllAboutTeethDCMS/ViewModels/AddAccountViewModel.vb
+++ AllAboutTeethDCMS/ViewModels/AddAccountViewModel.vb
@@ -27,4 +27,130 @@
             user.MiddleInitial = value
         End Set
     End Property
+
+    Public Property Birthdate As Date
+        Get
+            Return user.Birthdate
+        End Get
+        Set(value As Date)
+            user.Birthdate = value
+        End Set
+    End Property
+
+    Public Property AccountID As String
+        Get
+            Return user.AccountID
+        End Get
+        Set(value As String)
+            user.AccountID = value
+        End Set
+    End Property
+
+    Public Property Password As String
+        Get
+            Return user.Password
+        End Get
+        Set(value As String)
+            user.Password = value
+        End Set
+    End Property
+
+    Public Property Gender As String
+        Get
+            Return user.Gender
+        End Get
+        Set(value As String)
+            user.Gender = value
+        End Set
+    End Property
+
+    Public Property ContactNo As String
+        Get
+            Return user.ContactNo
+        End Get
+        Set(value As String)
+            user.ContactNo = value
+        End Set
+    End Property
+
+    Public Property Address As String
+        Get
+            Return user.Address
+        End Get
+        Set(value As String)
+            user.Address = value
+        End Set
+    End Property
+
+    Public Property EmailAddress As String
+        Get
+            Return user.EmailAddress
+        End Get
+        Set(value As String)
+            user.EmailAddress = value
+        End Set
+    End Property
+
+    Public Property Image As String
+        Get
+            Return user.Image
+        End Get
+        Set(value As String)
+            user.Image = value
+        End Set
+    End Property
+
+    Public Property Question1 As String
+        Get
+            Return user.Question1
+        End Get
+        Set(value As String)
+            user.Question1 = value
+        End Set
+    End Property
+
+    Public Property Question2 As String
+        Get
+            Return user.Question2
+        End Get
+        Set(value As String)
+            user.Question2 = value
+        End Set
+    End Property
+
+    Public Property Question3 As String
+        Get
+            Return user.Question3
+        End Get
+        Set(value As String)
+            user.Question3 = value
+        End Set
+    End Property
+
+    Public Property Answer1 As String
+        Get
+            Return user.Answer1
+        End Get
+        Set(value As String)
+            user.Answer1 = value
+        End Set
+    End Property
+
+    Public Property Answer2 As String
+        Get
+            Return user.Answer2
+        End Get
+        Set(value As String)
+            user.Answer2 = value
+        End Set
+    End Property
+
+    Public Property Answer3 As String
+        Get
+            Return user.Answer3
+        End Get
+        Set(value As String)
+            user.Answer3 = value
+        End Set
+    End Property
 End Class
