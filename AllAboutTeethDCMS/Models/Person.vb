14d92a159c56e03aeb8f3930ebf3095032b767d0
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-13 09:58:38 +0800

    Started Bindings.

--- AllAboutTeethDCMS/Models/Person.vb
+++ AllAboutTeethDCMS/Models/Person.vb
@@ -1,5 +1,14 @@
 ﻿Public Class Person
+    Private family_name As String = "Estrera"
+
     Public Property FamilyName As String
+        Get
+            Return family_name
+        End Get
+        Set(value As String)
+            family_name = value
+        End Set
+    End Property
     Public Property GivenName As String
     Public Property MiddleInitial As String
     Public Property Birthdate As Date
---
+++ AllAboutTeethDCMS/ViewModels/AddAccountViewModel.vb
@@ -0,0 +1,30 @@
+﻿Public Class AddAccountViewModel
+    Private user As New User
+
+    Public Property FamilyName As String
+        Get
+            Return user.FamilyName
+        End Get
+        Set(value As String)
+            user.FamilyName = value
+        End Set
+    End Property
+
+    Public Property GivenName As String
+        Get
+            Return user.GivenName
+        End Get
+        Set(value As String)
+            user.GivenName = value
+        End Set
+    End Property
+
+    Public Property MiddleInitial As String
+        Get
+            Return user.MiddleInitial
+        End Get
+        Set(value As String)
+            user.MiddleInitial = value
+        End Set
+    End Property
+End Class
