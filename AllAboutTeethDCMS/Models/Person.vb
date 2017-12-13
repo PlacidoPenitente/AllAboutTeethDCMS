835be84970a0c27cb5e6a7c93b4e59f07ec32a75
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-13 11:03:22 +0800

    Bindings pa!

--- AllAboutTeethDCMS/Models/Person.vb
+++ AllAboutTeethDCMS/Models/Person.vb
@@ -4,6 +4,10 @@
     Private m_middleinitial As String = "P"
     Private m_gender As String = "Male"
     Private m_birthdate As Date = Date.Now
+    Private m_contactno As String = "09568032571"
+    Private m_address As String = "Taytay, Rizal"
+    Private m_emailaddress As String = "estrerajaymark@ymail.com"
+    Private m_image As String = "none"
 
 
     Public Property FamilyName As String
@@ -52,8 +56,39 @@
     End Property
 
     Public Property ContactNo As String
+        Get
+            Return m_contactno
+        End Get
+        Set(value As String)
+            m_contactno = value
+        End Set
+    End Property
+
     Public Property Address As String
+        Get
+            Return m_address
+        End Get
+        Set(value As String)
+            m_address = value
+        End Set
+    End Property
+
     Public Property EmailAddress As String
+        Get
+            Return m_emailaddress
+        End Get
+        Set(value As String)
+            m_emailaddress = value
+        End Set
+    End Property
+
     Public Property Image As String
+        Get
+            Return m_image
+        End Get
+        Set(value As String)
+            m_image = value
+        End Set
+    End Property
 
 End Class
