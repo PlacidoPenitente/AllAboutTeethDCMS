6b1fc83e2b38fce9359cdc394e82b018af6dbfaf
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-22 09:58:43 +0800

    Added Password

--- AllAboutTeethDCMS/Pages/AddAccount.xaml.vb
+++ AllAboutTeethDCMS/Pages/AddAccount.xaml.vb
@@ -1,3 +1,8 @@
 ﻿Public Class AddAccount
-
+    Private Sub PasswordBox_PasswordChanged(sender As Object, e As RoutedEventArgs)
+        Dim passwordBox As PasswordBox = sender
+        Dim control As UserControl = CType(CType(CType(CType(passwordBox.Parent, StackPanel).Parent, StackPanel).Parent, Grid).Parent, UserControl)
+        Dim addAccountViewModel As AddAccountViewModel = control.DataContext
+        addAccountViewModel.Password = passwordBox.Password
+    End Sub
 End Class
