1de755fcb486fd07d9e1b49664f59c0dd1ea6c59
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-22 10:05:56 +0800

    Compare Passwords

--- AllAboutTeethDCMS/Pages/AddAccount.xaml.vb
+++ AllAboutTeethDCMS/Pages/AddAccount.xaml.vb
@@ -4,5 +4,17 @@
         Dim control As UserControl = CType(CType(CType(CType(passwordBox.Parent, StackPanel).Parent, StackPanel).Parent, Grid).Parent, UserControl)
         Dim addAccountViewModel As AddAccountViewModel = control.DataContext
         addAccountViewModel.Password = passwordBox.Password
+        comparePasswords(password.Password, confirm.Password)
+    End Sub
+
+    Public Function comparePasswords(first As String, second As String) As Boolean
+        If first = second Then
+            Return True
+        End If
+        Return False
+    End Function
+
+    Private Sub PasswordBox_PasswordChanged_1(sender As Object, e As RoutedEventArgs)
+        comparePasswords(password.Password, confirm.Password)
     End Sub
 End Class
